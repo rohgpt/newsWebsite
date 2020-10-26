59444c8892ba41ea3ec95f1afa005e1e8807edf9
Rohit Kumar Gupta <rohitgupta.dbs@gmail.com>
2020-10-26 09:53:02 +0530

    Updates

---
+++ precache-manifest.b25d56b4485bc09df82e584dcf2754be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43872c4a9b21f4d77bc32f98585f4cdd",
+    "url": "/newsWebsite/index.html"
+  },
+  {
+    "revision": "3c04a65f2158c26816ff",
+    "url": "/newsWebsite/static/css/main.7b9bd383.chunk.css"
+  },
+  {
+    "revision": "ec8676b9fa8e1d03d2c9",
+    "url": "/newsWebsite/static/js/2.ad884b90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/newsWebsite/static/js/2.ad884b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c04a65f2158c26816ff",
+    "url": "/newsWebsite/static/js/main.1077f5ae.chunk.js"
+  },
+  {
+    "revision": "64ab4033c5a23d9ccb27",
+    "url": "/newsWebsite/static/js/runtime-main.25a6f90b.js"
+  }
+]);
